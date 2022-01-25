6f15c6c694c2cd2521fe42394639f9b6e152e670
mirim <alfla6528@naver.com>
2022-01-26 04:46:32 +0900

    사칙연산

---
+++ solutions/10869.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const number = input.toString().split(" ");
+
+const a = parseInt(number[0]);
+const b = parseInt(number[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
